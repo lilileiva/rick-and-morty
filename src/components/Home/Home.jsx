4996ece6eeb0baa605b55cab0d8ed7c9967b1a5d
lilileiva <liliana-carmen@hotmail.com>
2023-07-01 16:30:43 -0300

    home animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import './Home.scss'
-// import Intro from '../../img/'
 import intro from '../../img/rick-and-morty-intro.mp4'
+import pepino from '../../img/rick-and-morty-pepino.png'
 
 
 function Home() {
@@ -9,6 +9,17 @@ function Home() {
             <div className='home'>
                 <video autoPlay preload='auto' loop controls src={intro} width='100%'>
                 </video>
+            </div>
+            <div className="pepinos">
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
+                <img src={pepino} alt='pepino' className='pepino' />
             </div>
         </div>
     );
